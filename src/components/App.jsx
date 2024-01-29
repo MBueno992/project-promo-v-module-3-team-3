85ac82eb628ccb6fa72f84f57993fc353bc0f73a
carodriguezp <crpzgz92@gmail.com>
2024-01-29 17:18:04 +0100

    create font families and color variables, finish style of header and footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import logoAdalab from '../images/logo-adalab.png';
 import { useState } from 'react';
 
 function App() {
-  
+
   const [projectName, setProjectName] = useState("");
   const [projectSlogan, setProjectSlogan] = useState("");
   const [projectTechnologies, setProjectTechnologies] = useState("");
@@ -15,8 +15,8 @@ function App() {
   const [projectDesc, setProjectDesc] = useState("");
   const [authorName, setAuthorName] = useState("");
   const [authorJob, setAuthorJob] = useState("");
- /*  const [authorPhoto, setAuthorPhoto] = useState(""); */
-/*   const [image, setImage] = useState(""); */
+  /*  const [authorPhoto, setAuthorPhoto] = useState(""); */
+  /*   const [image, setImage] = useState(""); */
 
 
 
@@ -43,36 +43,37 @@ function App() {
           <h2>
             Escaparate en línea para recoger ideas a través de la tecnología
           </h2>
-          <button className="header__title--btn">Nuevo proyecto</button>
+          <button className="header__title--new-btn">Nuevo proyecto</button>
+          <button className="header__title--see-btn">Ver proyectos</button>
         </div>
       </header>
 
       <main className="main">
         <section className="preview">
           <img className="preview__image" src={project} alt="" />
-        
-        <section className="author">
-          <section className="author__project">
-            <p className="author__project--subtitle">Personal Project Card</p>
-            <hr className="author__project--line" />
-            <h2 className="author__project--title">Elegant Workspace</h2>
-            <p className="author__project--slogan">Diseños Exclusivos</p>
-            <p className="author__project--description">Product Description</p>
-            <p className="author__project--desc">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
-              delectus? Voluptates at hic aliquam porro ad suscipit
-            </p>
-            <section className="author__technologies">
-              <p className="author__technologies--text">React JS - MongoDB </p>
+
+          <section className="author">
+            <section className="author__project">
+              <p className="author__project--subtitle">Personal Project Card</p>
+              <hr className="author__project--line" />
+              <h2 className="author__project--title">Elegant Workspace</h2>
+              <p className="author__project--slogan">Diseños Exclusivos</p>
+              <p className="author__project--description">Product Description</p>
+              <p className="author__project--desc">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero,
+                delectus? Voluptates at hic aliquam porro ad suscipit
+              </p>
+              <section className="author__technologies">
+                <p className="author__technologies--text">React JS - MongoDB </p>
+              </section>
+            </section>
+
+            <section className="author__info">
+              <img className="author__info--image" src={user} alt="" />
+              <p className="author__info--job">Full Stack Developer</p>
+              <p className="author__info--name">Emmelie Björklund</p>
             </section>
           </section>
-
-          <section className="author__info">
-            <img className="author__info--image" src={user} alt="" />
-            <p className="author__info--job">Full Stack Developer</p>
-            <p className="author__info--name">Emmelie Björklund</p>
-          </section>
-        </section>
         </section>
 
         <section className="form">
