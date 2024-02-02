31b51c6cf2fbc4d56a02f908359d7f710656ade3
Mbueno992 <mbueno_92@hotmail.com>
2024-02-02 14:02:41 +0100

    change url text

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-promo-v-module-3-team-3',
+  base: 'https://mbueno992.github.io/project-promo-v-module-3-team-3/',
 });
